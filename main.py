71b434b4b4a421fe57f78b8928216247b3c813f4
joalricha869 <83835381+joalricha869@users.noreply.github.com>
2022-02-07 22:20:15 -0500

    Updated Code to version 2.3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-import subprocess
-import shlex
-# Original 3 Libaries from Termitrhon 1
+import socket
+import getpass
+from uuid import getnode as get_mac
+mac = get_mac()
 import string
 from random import *
 import time
@@ -11,7 +12,8 @@ import platform
 import subprocess
 import re
 import uuid
-
+# Made in python
+# collaborated with https://github.com/BigBoyTaco/ for this little update
 # Made in Python, with PyCharm and OnlineGDB
 # Compiled on desktop in Visual Studio 2019
 # Thanks for green1490#2863 for helping me with arguments
@@ -37,7 +39,7 @@ print('The source is here')
 print("Type in 'help' for the command list.")
 print("")
 
-
+hostname = socket.gethostname()
 
 def listToString(s):
     str1 = ""
@@ -49,30 +51,19 @@ def listToString(s):
 current_dir = os.getcwd()
 
 commands = '''dir (Shows files in current directory)
-cd (Go to specific directory in OS File System) (BETA STAGE DOESN'T WORK)
 exit (Exits Termithron)
-getmac (Retrieves the Physical MAC Address of The Device)
-getip (Gets IP Address of Device)
-getsysteminfo (Gets relevant system info)
+ip (Gives you your IP)
+hostname (Gives you your Computer's ID)
+mac (Retrieves the Physical MAC Address of The Device)
+ping (lets you ping a website)
 calc (A simple calculator)
 passgen (A very efficient password generator)
+sysinfo (Gets relevant system info)
 ver (Reports Termithron Version)
-'''
-
-newHelp = '''dir (Shows files in current directory)
-cd (Go to specific directory in OS File System) (BETA STAGE DOESN'T WORK)
-exit (Exits Termithron)
-getmac (Retrieves the Physical MAC Address of The Device)
-getip (Gets IP Address of Device)
-getsysteminfo (Gets relevant system info)
-calc (A simple calculator)
-passgen (A very efficient password generator)
-ver (Reports Termithron Version)
-dev (Enables DEV Mode)
 '''
 
 def cdCmd():
-    print("Sorry, cd command doesn't update so no.")
+    print("sorry, the cd command doesnt update so no ")
     main()
     '''
     path = input('Directory: ')
@@ -89,48 +80,49 @@ def cdCmd():
         '''
 
 
-
 def whatiscommand():
+    #help command
     if cmd == 'help':
         print(commands)
         main()
-
+    #ls command
     elif cmd == 'dir':
         print(os.listdir(current_dir))
         main()
-
+    #cd command
     elif cmd == "cd":
         global args
         args = cmd
         cdCmd()
+    #exit command
     elif cmd == 'exit':
         exit()
-    elif cmd == 'getmac':
-        getmac()
-        main()
-    elif cmd == "calc":
-        calc()
-        main()
-    elif cmd == "passgen":
-        passGen()
-        main()
-    elif cmd == 'getip':
+    #ip command
+    elif cmd == 'ip':
         print("Your IP Address is " + getip())
         main()
-    elif cmd == 'getsysteminfo':
+    #hostname command
+    elif cmd == 'hostname':
+        uname = platform.uname()
+        print(hostname)
+        main()
+    elif cmd == "mac":
+        getmac()
+        main()
+    elif "ping" in cmd:
+        os.system(cmd)
+    elif cmd == "calc":
+        calc()
+    elif cmd == "passgen":
+        passGen()
+    elif cmd == "sysinfo":
         getSystemInfo()
         main()
-    elif cmd == 'ver':
+    elif cmd == "ver":
         ver()
         main()
-    elif cmd == "dev":
-        debugSecrets()
-    elif cmd == "devHelp":
-        devHelp()
-        main()
     else:
-        print("Invalid Command or Syntax... Use the 'help' command for more info")
-        main()
+        error()
 
 
 def main():
@@ -139,29 +131,20 @@ def main():
     whatiscommand()
 
 def ver():
-    print("Termithron 2.3 RELEASE_CANDIDATE_2   ! PRE-RELEASE VERSION OF TERMITHRON 2.0 !")
+    print("Termithron 2.3")
     print("(C) 2022 joalricha869, idkDwij All Rights Reserved.")
 
-def debugSecrets():
-    print("EXPERIMENTIAL FEATURES AHEAD!")
-    print("These features are in development and are NOT complete. Some may work, some may not work at all.")
-    enableDevMode = input("ENABLE EXPERIMENTAL FEATURES? ")
-    if enableDevMode == "yes":
-        print("Experminental Features Enabled. NEW COMMAND: devHelp")
-        main()
-    else:
-        print("No changes were made to the Terminal.")
-        main()
-
-def getmac():
-    import re, uuid
-    print ("The MAC address of this Device is : ", end="")
-    print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
-
-def devHelp():
-    print(newHelp)
-
-
+def getSystemInfo():
+    print("="*40, "System Information", "="*40)
+    uname = platform.uname()
+    print(f"System: {uname.system}")
+    print(f"Node Name: {uname.node}")
+    print(f"Release: {uname.release}")
+    print(f"Version: {uname.version}")
+    print(f"Machine: {uname.machine}")
+    print(f"Processor: {uname.processor}")
+    print("System Info Retrieved!")
+    
 def calc():
     def add(x, y):
         return x + y
@@ -195,6 +178,7 @@ def calc():
             else:
                 print("Invalid Input")
                 main()
+
 def passGen():
         characters = string.ascii_letters + string.punctuation  + string.digits
         password =  "".join(choice(characters) for x in range(randint(8, 16)))
@@ -204,6 +188,12 @@ def passGen():
             passGen()
         else:
             main()
+
+def getmac():
+    import re, uuid
+    print ("The MAC address of this Device is : ", end="")
+    print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
+
 def getip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:       
@@ -214,15 +204,12 @@ def getip():
     finally:
         st.close()
     return IP
-def getSystemInfo():
-    print("="*40, "System Information", "="*40)
-    uname = platform.uname()
-    print(f"System: {uname.system}")
-    print(f"Node Name: {uname.node}")
-    print(f"Release: {uname.release}")
-    print(f"Version: {uname.version}")
-    print(f"Machine: {uname.machine}")
-    print(f"Processor: {uname.processor}")
-    print("System Info Retrieved!")
-lsdir = current_dir
+
+def error():
+    print("'" + str(cmd) + "'" + ''' is not recognized as an internal or external command''')
+    main()
+
+        
+
+
 main()
