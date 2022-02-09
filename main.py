c69c6110cd4792cb3cd8be9fe81b3a68f045ccfd
Nathan <90655713+BigBoyTaco@users.noreply.github.com>
2022-02-09 11:40:17 -0500

    fixed ping command and other stuff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#imports
 from __future__ import division
 import os
 import socket
 import getpass
 from uuid import getnode as get_mac
-mac = get_mac()
 import string
 import random
 from random import choice
@@ -20,6 +20,7 @@ import math
 import signal
 import sys
 import itertools
+
 # Made in python
 # collaborated with https://github.com/BigBoyTaco/ for this little update
 # Made in Python, with PyCharm and OnlineGDB
@@ -39,15 +40,22 @@ joalricha = '''
     J   O   O  AAAAA  L    RRRR   I  C     H  H  AAAAA
   JJJ    OOO   A   A  LLL  R   R  I   CCC  H  H  A   A
 '''
-print('The Python based terminal by:' + idkdwij + 'it says idkdwij tho')
+print('The Python based terminal by:' + idkdwij + 'it says idkdwij tho https://github.com/IdkDwij')
 print(" ")
-print('Expanded by:' + joalricha + 'it says joalricha')
+print('Expanded by:' + joalricha + 'it says joalricha https://github.com/joalricha869')
+print('edited by https://github.com/BigBoyTaco')
 print(" ")
 print('The source is here')
 print("Type in 'help' for the command list.")
 print("")
 
+#setup
+#gets hostname
 hostname = socket.gethostname()
+#gets mac address
+mac = get_mac()
+#gets current directory
+current_dir = os.getcwd()
 
 def listToString(s):
     str1 = ""
@@ -55,27 +63,25 @@ def listToString(s):
         str1 += ele
     return str1
 
-
-current_dir = os.getcwd()
-
 commands = '''dir (Shows files in current directory)
-exit (Exits Termithron)
-ip (Gives you your IP)
-hostname (Gives you your Computer's ID)
-mac (Retrieves the Physical MAC Address of The Device)
-ping (lets you ping a website)
-calc (A simple calculator)
-passgen (A very efficient password generator)
-sysinfo (Gets relevant system info)
-test (Tests Termithron Sample Command)
-mp3search (Searches your File System for mp3 files)
-mp4search (Searches your File System for mp4 files)
-pysearch (Searches your File System for py files)
-docxsearch (Searches your File System for docx files)
-mailgen (Generates dummy E-Mail Addresses)
-ver (Reports Termithron Version)
+1. exit (Exits Termithron)
+2. ip (Gives you your IP)
+3. hostname (Gives you your Computer's ID)
+4. mac (Retrieves the Physical MAC Address of The Device)
+5. ping (lets you ping a website)
+6. calc (A simple calculator)
+7. passgen (A very efficient password generator)
+8. sysinfo (Gets relevant system info)
+9. test (Tests Termithron Sample Command)
+10. mp3search (Searches your File System for mp3 files)
+11. mp4search (Searches your File System for mp4 files)
+12. pysearch (Searches your File System for py files)
+13. docxsearch (Searches your File System for docx files)
+14. mailgen (Generates dummy E-Mail Addresses)
+15. ver (Reports Termithron Version)
 '''
 
+#cd command (work in progress)
 def cdCmd():
     print("sorry, the cd command doesnt update so no ")
     main()
@@ -94,7 +100,7 @@ def cdCmd():
         main()
         '''
 
-
+#what command was requested
 def whatiscommand():
     #help command
     if cmd == 'help':
@@ -121,52 +127,66 @@ def whatiscommand():
         uname = platform.uname()
         print(hostname)
         main()
+    #get mac adress
     elif cmd == "mac":
         getmac()
         main()
+    #ping
     elif "ping" in cmd:
         os.system(cmd)
+        main()
+    #calculator
     elif cmd == "calc":
         calc()
+    #password genorator
     elif cmd == "passgen":
         passGen()
+    #system information
     elif cmd == "sysinfo":
         getSystemInfo()
         main()
+    #version
     elif cmd == "ver":
         ver()
         main()
+    #test
     elif cmd == "test":
         testFunc()
         main()
+    #mp3 file searh
     elif cmd == "mp3search":
         mp3search()
         main()
+    #mp4 file search
     elif cmd == "mp4search":
         mp3search()
         main()
+    #python file search
     elif cmd == "pysearch":
         pysearch()
         main()
+    #word doc search
     elif cmd == "docxsearch":
         docxsearch()
         main()
+    #email genorator
     elif cmd == "mailgen":
         mailGen()
         main()
+    #else
     else:
         error()
 
-
+#define functions
 def main():
     global cmd
     cmd = input(current_dir + '>')
     whatiscommand()
-
+#version function
 def ver():
     print("Termithron BETA 3.0 Codename: PyTermi3")
     print("(C) 2022 joalricha869, idkDwij All Rights Reserved.")
-
+#system information function
 def getSystemInfo():
     print("="*40, "System Information", "="*40)
     uname = platform.uname()
@@ -177,7 +197,7 @@ def getSystemInfo():
     print(f"Machine: {uname.machine}")
     print(f"Processor: {uname.processor}")
     print("System Info Retrieved!")
-    
+#calculator function
 def calc():
     def add(x, y):
         return x + y
@@ -211,7 +231,7 @@ def calc():
             else:
                 print("Invalid Input")
                 main()
-
+#password genorator function
 def passGen():
         characters = string.ascii_letters + string.punctuation  + string.digits
         password =  "".join(choice(characters) for x in range(randint(8, 16)))
@@ -221,12 +241,12 @@ def passGen():
             passGen()
         else:
             main()
-
+#mac address function
 def getmac():
     import re, uuid
     print ("The MAC address of this Device is : ", end="")
     print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
-
+#get ip function
 def getip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:       
@@ -238,13 +258,18 @@ def getip():
         st.close()
     return IP
 
+#error function
 def error():
-    print("'" + str(cmd) + "'" + ''' is not recognized as an internal or external command''')
-    main()
-
+    if(cmd == ""):
+        main()
+    else:
+        print("'" + str(cmd) + "'" + ''' is not recognized as an internal or external command''')
+        print("for more help go https://github.com/joalricha869/Termithon-3.0 or https://github.com/IdkDwij/Termithon")
+        main()
+#test function
 def testFunc():
-    print("If this command works, then your Termithron is fine.")
-
+    print("If this command works, then your Termithron is fine... maybe")
+#mp3 search function
 def mp3search():
     rootPath = '/'
     pattern = '*.mp3'
@@ -252,6 +277,7 @@ def mp3search():
     for root, dirs, files in os.walk(rootPath):
         for filename in fnmatch.filter(files, pattern):
             print( os.path.join(root, filename))
+#mp4 search function
 def mp4search():
     rootPath = '/'
     pattern = '*.mp4'
@@ -259,6 +285,7 @@ def mp4search():
     for root, dirs, files in os.walk(rootPath):
         for filename in fnmatch.filter(files, pattern):
             print( os.path.join(root, filename))
+#py search function
 def pysearch():
     rootPath = '/'
     pattern = '*.py'
@@ -266,6 +293,7 @@ def pysearch():
     for root, dirs, files in os.walk(rootPath):
         for filename in fnmatch.filter(files, pattern):
             print( os.path.join(root, filename))
+#word doc search
 def docxsearch():
     rootPath = '/'
     pattern = '*.docx'
@@ -273,7 +301,7 @@ def docxsearch():
     for root, dirs, files in os.walk(rootPath):
         for filename in fnmatch.filter(files, pattern):
             print( os.path.join(root, filename))
-
+#email genorator
 def mailGen():
     extensions = ['com']
     domains = ['gmail','yahoo','comcast','verizon','charter','hotmail','outlook','frontier','icloud','yandex']
