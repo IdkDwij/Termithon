fcfc7937757bc8c391b69fdf05b8bae78c45f7f9
Jose <joalricha869@gmail.com>
2022-03-01 20:04:06 -0500

    Updated code to 1.2.2.1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import platform
 import fnmatch
 import webbrowser
 from time import sleep
+import cmath
 print("="*40, "PyPrompt", "="*40)
 joalricha = '''
     _             _      _      _            ___    __ ___  
@@ -63,53 +64,102 @@ def listToString(s):
         str1 += ele
     return str1
 commands = '''
- _______        _______                                       _     ____  ____        __           
-|_   __ \      |_   __ \                                     / |_  |_   ||   _|      [  |          
-  | |__) |_   __ | |__) |_ .--.   .--.   _ .--..--.  _ .--. `| |-'   | |__| |  .---.  | | _ .--.   
-  |  ___/[ \ [  ]|  ___/[ `/'`\]/ .'`\ \[ `.-. .-. |[ '/'`\ \| |     |  __  | / /__\\ | |[ '/'`\ \ 
- _| |_    \ '/ /_| |_    | |    | \__. | | | | | | | | \__/ || |,   _| |  | |_| \__., | | | \__/ | 
-|_____| [\_:  /|_____|  [___]    '.__.' [___||__||__]| ;.__/ \__/  |____||____|'.__.'[___]| ;.__/  
-         \__.'                                      [__|                                 [__|      
-1. dir (Shows files in current directory)
-2. exit (Exits the terminal)
-3. ip (Gives you your IP)
-4. hostname (Gives you your Computer's ID)
-5. mac (Retrieves the Physical MAC Address of The Device)
-6. ping (lets you ping a website UPDATE: NOW ADDED!) 
-7. calc (A simple calculator)
-8. passgen (A very efficient password generator)
-9. sysinfo (Gets relevant system info)
-10. test (Tests PyPrompt Sample Command)
-11. mp3search (Searches your File System for mp3 files)
-12. mp4search (Searches your File System for mp4 files)
-14. pysearch (Searches your File System for py files)
-15. docxsearch (Searches your File System for docx files)
-16. mailgen (Generates dummy E-Mail Addresses)
-17. ver (Reports PyPrompt Version)
-18. sudo (This uses the REAL sudo mode.) ONLY WORKS ON LINUX OR WSL
-19. clear (Clears screen)
-20. mkdir (Creates a folder)
-21. del (deletes a file or directory) ONLY WORKS ON WINDOWS
-22. rm (deletes a file or directory) ONLY WORKS ON LINUX OR MACOS
-23. loadbarTest (Tests the loadbar)
-24. diskpart (Opens up the real diskpart) ONLY WORKS ON WINDOWS
-25. format (Formats a drive) ONLY WORKS ON WINDOWS
-26. history (Shows all your command history) ONLY WORKS ON LINUX OR MACOS
-27. hash (Displays program locations) ONLY WORKS ON LINUX OR MACOS
-28. color (Changes the color of text) ONLY WORKS ON WINDOWS
-29. intro (Displays initial text)
-30. py (Opens up the Python shell, REQUIRES PYTHON TO BE INSTALLED!) ONLY WORKS ON WINDOWS
-31. python (Alternate solution to the 'py' command) ONLY WORKS ON WINDOWS
-32. python3 (Opens the Python shell, REQUIRES PYTHON 3 TO BE INSTALLED!) ONLY WORKS ON LINUX
-33. pip (Opens up pip) REQUIRES PYTHON AND PIP TO BE INSTALLED!
-34. wsl (Opens up the Linux shell, REQUIRES THE WINDOWS SUBSYSTEM FOR LINUX) ONLY WORKS ON WINDOWS!
-35. apt (Command used to install package dependencies) ONLY WORKS ON LINUX
-36. git (Opens up git, REQUIRES GIT TO BE INSTALLED!)
-37. bash (Launches the bash terminal) ONLY WORKS ON LINUX OR WINDOWS (with wsl)
-38. csh (Launches the C shell; REQUIRES csh to be installed) ONLY WORKS ON LINUX OR THE WSL
-39. zsh (Launches the Z shell; REQUIRES zsh to be installed) ONLY WORKS ON LINUX OR THE WSL
-40. unrar (Extracts files from a .rar file; REQUIRES unrar binary to be installed) ONLY WORKS ON LINUX
-41. su (Enable or disable the sudo command) ONLY WORKS ON LINUX OR WSL
+  _____       _                       _           _ 
+ |_   _|     | |                     | |         | |
+   | |  _ __ | |_ ___  __ _ _ __ __ _| |_ ___  __| |
+   | | | '_ \| __/ _ \/ _` | '__/ _` | __/ _ \/ _` |
+  _| |_| | | | ||  __/ (_| | | | (_| | ||  __/ (_| |
+ |_____|_| |_|\__\___|\__, |_|  \__,_|\__\___|\__,_|
+                       __/ |                        
+                      |___/                         
+
+1. ip (Gives you your IP)
+2. hostname (Gives you your Computer's ID)
+3. mac (Retrieves the Physical MAC Address of The Device)
+4. ping (lets you ping a website UPDATE: NOW ADDED!) not
+5. calc (A simple calculator)
+6. passgen (A very efficient password generator)
+7. sysinfo (Gets relevant system info)
+8. test (Tests PyPrompt Sample Command)
+9. mp3search (Searches your File System for mp3 files)
+10. mp4search (Searches your File System for mp4 files)
+11. pysearch (Searches your File System for py files)
+12. docxsearch (Searches your File System for docx files)
+13. mailgen (Generates dummy E-Mail Addresses)
+14. ver (Reports PyPrompt Version)
+15. clear (Clears screen)
+16. loadbarTest (Tests the loadbar)
+17. intro (Displays initial text)
+18. sqrt (Enter a number and it will calculate the square root)
+19. date (Displays date)
+
+
+   \  |        |  |   _)       |         |     _|               
+  |\/ |  |  |  |   _|  |  _ \  |   _` |   _|   _| _ \   _| ` \  
+ _|  _| \_,_| _| \__| _| .__/ _| \__,_| \__| _| \___/ _| _|_|_| 
+                        _|                                      
+
+1. git (Opens up git, REQUIRES GIT TO BE INSTALLED!)
+2. pip (Opens up pip) REQUIRES PYTHON AND PIP TO BE INSTALLED!
+3. dir (Shows files in current directory)
+4. exit (Exits the terminal)
+5. mkdir (Creates a folder)
+
+
+  _      _                    _    _      _       
+ | |    (_)                  | |  | |    | |      
+ | |     _ _ __  _   ___  __ | |__| | ___| |_ __  
+ | |    | | '_ \| | | \ \/ / |  __  |/ _ \ | '_ \ 
+ | |____| | | | | |_| |>  <  | |  | |  __/ | |_) |
+ |______|_|_| |_|\__,_/_/\_\ |_|  |_|\___|_| .__/ 
+                                           | |    
+                                           |_|    
+                                           
+1. sudo (This uses the REAL sudo mode.) ONLY WORKS ON LINUX OR WSL
+2. rm (deletes a file or directory) ONLY WORKS ON LINUX OR WSL
+3. history (Shows all your command history) ONLY WORKS ON LINUX OR WSL
+4. hash (Displays program locations) ONLY WORKS ON LINUX OR WSL
+5. python3 (Opens the Python shell, REQUIRES PYTHON 3 TO BE INSTALLED!) ONLY WORKS ON LINUX OR WSL
+6. apt (Command used to install package dependencies) ONLY WORKS ON LINUX OR WSL
+7. bash (Launches the bash terminal) ONLY WORKS ON LINUX OR WINDOWS (with wsl)
+8. csh (Launches the C shell; REQUIRES csh to be installed) ONLY WORKS ON LINUX OR WSL
+9. zsh (Launches the Z shell; REQUIRES zsh to be installed) ONLY WORKS ON LINUX OR WSL
+10. unrar (Extracts files from a .rar file; REQUIRES unrar binary to be installed) ONLY WORKS ON LINUX OR WSL
+11. su (Enable or disable the sudo command) ONLY WORKS ON LINUX OR WSL
+12. cat (Creates a blank file) ONLY WORKS ON LINUX OR WSL
+
+ __          ___           _                     _    _      _       
+ \ \        / (_)         | |                   | |  | |    | |      
+  \ \  /\  / / _ _ __   __| | _____      _____  | |__| | ___| |_ __  
+   \ \/  \/ / | | '_ \ / _` |/ _ \ \ /\ / / __| |  __  |/ _ \ | '_ \ 
+    \  /\  /  | | | | | (_| | (_) \ V  V /\__ \ | |  | |  __/ | |_) |
+     \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/ |_|  |_|\___|_| .__/ 
+                                                              | |    
+                                                              |_|    
+
+
+1. del (deletes a file or directory) ONLY WORKS ON WINDOWS
+2. diskpart (Opens up the real diskpart) ONLY WORKS ON WINDOWS
+3. format (Formats a drive) ONLY WORKS ON WINDOWS
+4. color (Changes the color of text) ONLY WORKS ON WINDOWS
+5. py (Opens up the Python shell, REQUIRES PYTHON TO BE INSTALLED!) ONLY WORKS ON WINDOWS
+6. python (Alternate solution to the 'py' command) ONLY WORKS ON WINDOWS
+7. wsl (Opens up the Linux shell, REQUIRES THE WINDOWS SUBSYSTEM FOR LINUX) ONLY WORKS ON WINDOWS!
+8. cmd (Opens the Command Prompt) ONLY WORKS ON WINDOWS
+9. ipconfig (Gives info about your IPV4 or IPV6 address, ip address, mac address) ONLY WORKS ON WINDOWS!
+
+                        ____   _____   _    _      _       
+                       / __ \ / ____| | |  | |    | |      
+  _ __ ___   __ _  ___| |  | | (___   | |__| | ___| |_ __  
+ | '_ ` _ \ / _` |/ __| |  | |\___ \  |  __  |/ _ \ | '_ \ 
+ | | | | | | (_| | (__| |__| |____) | | |  | |  __/ | |_) |
+ |_| |_| |_|\__,_|\___|\____/|_____/  |_|  |_|\___|_| .__/ 
+                                                    | |    
+                                                    |_|    
+
+1. brew (The homebrew command) ONLY WORKS ON MACOS
+2. softwareupdate (Checks for either macOS updates or software updates) ONLY WORKS ON MACOS
+3. airport (Manage Apple AirPort) MIGHT REQUIRE zsh...   REQUIRES MACOS
 '''
 
 def whatiscommand():
@@ -237,6 +287,30 @@ def whatiscommand():
     elif "su" in cmd:
         os.system(cmd)
         main()
+    elif "cmd" in cmd:
+        os.system(cmd)
+        main()
+    elif "brew" in cmd:
+        os.system(cmd)
+        main()
+    elif "softwareupdate" in cmd:
+        os.system(cmd)
+        main()
+    elif "airport" in cmd:
+        os.system(cmd)
+        main()
+    elif "ipconfig" in cmd:
+        os.system(cmd)
+        main()
+    elif "cat" in cmd:
+        os.system(cmd)
+        main()
+    elif "sqrt" in cmd:
+        sqrt()
+        main()
+    elif "date" in cmd:
+        date()
+        main()
     else:
         error()
 def main():
@@ -299,9 +373,10 @@ def passGen():
         else:
             main()
 def getmac():
-    import re, uuid
+    import uuid
     print ("The MAC address of this Device is : ", end="")
-    print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
+    print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+    for ele in range(0,8*6,8)][::-1]))
 def getip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:       
@@ -366,20 +441,20 @@ def mailGen():
     else:
         main()
 def progressbar():
-    def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='>'):
+    def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
         percent = ('{0:.' + str(decimals) + 'f}').format(100 * (iteration/float(total)))
         filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
+        bar = fill * filledLength + '▒' * (length - filledLength)
         print(f'\r{prefix} |{bar}| {percent}% {suffix}',end='\r')
         if iteration == total:
             print()
     items = list(range(0, 50))
     l = len(items)
     
-    loadbar(0, l, prefix='Progress', suffix='Complete', length=l)
+    loadbar(0, l, prefix='Generating...', suffix='Done!', length=l)
     for i, item in enumerate(items):
         sleep(0.1)
-        loadbar(i + 1, l, prefix='Progress', suffix='Complete', length=l)
+        loadbar(i + 1, l, prefix='Generating...', suffix='Done!', length=l)
 
 def intro():
     print("=" * 40, "PyPrompt", "=" * 40)
@@ -387,13 +462,25 @@ def intro():
     print(" ")
     print('Thanks to ' + taco + 'for help  https://github.com/BigBoyTaco')
     print(" ")
-    print('Based on Termithron by' + dwij + 'https://github.com/IdkDwij/Termithron')
+    print('Based on Termithron Shell by' + dwij + 'https://github.com/IdkDwij/Termithron')
     print(" ")
     print('The source is here')
     print("Type in 'help' for the command list.")
     print("")
 
-y = "1.2.1"
+def sqrt():
+    num = float(input('Enter a number: '))
+    num_sqrt = num ** 0.5
+    print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+def date():
+    from datetime import date
+    today = date.today()
+    d2 = today.strftime("%B %d, %Y")
+    print("Today's date is:", d2)
+    main()
+
+y = "1.2.2.1"
 
 
 main()
